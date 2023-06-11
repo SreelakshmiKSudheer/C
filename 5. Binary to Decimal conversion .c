21820eec157c31ab3764ea58dc58f7683c003c18
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2023-06-11 11:13:42 +0530

    Update 5. Binary to Decimal conversion .c

--- 5. Binary to Decimal conversion .c
+++ 5. Binary to Decimal conversion .c
@@ -2,28 +2,42 @@
 
 int main()
 {
-	int bi_num,dec_num = 0,digit,multiplier = 1; 		//v_input = 1,
+	int bi_num,dec_num,bit,multiplier = 1; 		
+	char choice = 'y';
 	
-	printf("Input a binary number: ");
-	scanf("%d",&bi_num);
-	
-	while(bi_num)
+	while (choice == 'y' || choice == 'Y')
 	{
-		
-		//v_input = 1;
-		digit = bi_num % 10;
-		if(!(digit == 0 || digit == 1))
+
+		printf("Input a binary number: ");
+		scanf("%d",&bi_num);
+	
+		while(bi_num)
 		{
-			printf("The given input is not a binary number.\nEnter again: ");
-			scanf("%d",&bi_num);
-			//v_input = 0;
-			continue;
+			bit = bi_num % 10;
+			if(!(bit == 0 || bit == 1))
+			{
+				printf("Not a binary number.\nEnter again: ");
+				scanf("%d",&bi_num);
+				dec_num = 0;
+				multiplier = 1;
+
+				continue;
+
+				
+			}
+			else
+			{
+				dec_num += bit * multiplier;
+				multiplier *= 2;
+				bi_num /= 10;
+
+			}
+
 		}
-		dec_num = dec_num + (digit * multiplier);
-		multiplier *= 2;
-		bi_num /= 10;
-		
-		
+	printf("Equivalent decimal number = %d\n",dec_num);		
+	printf("Do you want to repea(y/n)t: ");
+	scanf(" %c",&choice);
 	}
-	printf("Equivalent decimal number = %d",dec_num);
+	
+
 }
