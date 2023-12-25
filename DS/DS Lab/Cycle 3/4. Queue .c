acf2ccae3c3c45b925ee31cbf8035f4e52b68c65
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2023-12-25 20:13:12 +0530

    Update 4. Queue .c

--- DS/DS Lab/Cycle 3/4. Queue .c
+++ DS/DS Lab/Cycle 3/4. Queue .c
@@ -1,78 +1,83 @@
 #include <stdio.h>
 
-#define SIZE 3
-#define INVALID -9999
+#define SIZE 5
+#define INVALID -99999
 
-int Q[SIZE],front,rear;
+int Q[SIZE],front = -1,rear = -1;
 
 void enqueue(int item)
 {
-    if(rear == SIZE - 1)
-        printf("Q full, Coulcn't insert item\n");
-    else
-    {
-        if(front == -1)
-            front = 0;
-        Q[++rear] = item;
+	if(rear == SIZE - 1)
+		printf("Queue overflow\n");
+	else
+	{
+                if(front == -1)
+                        front = 0;
+                Q[++rear] = item;
+        }
 
-    }
 }
 
 int dequeue()
 {
-    if (front == -1)
-        return INVALID;
-    else
-    {
-        if(front == rear)
-        {
-            front = -1;
-            rear = -1;
-        }
-        return Q[front--];
-    }
+	int item;
+	if(front == -1){
+		
+		return INVALID;
+	}
+	else
+	{
+		item = Q[front++];
+		if(front == rear + 1)
+			front = rear = -1;
+		return item;
+	}
 }
 
 void display()
 {
-    if (front == -1)
-        printf("Q empty\n");
-    else
-    {
-        int i;
-        
-        for(i = front; i <= rear; i++)
-            printf("%d ",Q[i]);
-        printf("\n");
-    }
+	int i;
+
+	if(front == -1)
+		printf("Queue is empty\n");
+	else
+	{
+		for(i = front; i <= rear; i++)
+			printf("%d ",Q[i]);
+		printf("\n");
+	}
 }
+
 int main()
 {
-    int choice,item;
-    front = -1, rear = -1;
-    printf("Press\n1] Insert\n2] Delete\n3] Display\n4] Quit\n");
 
-    do
-    {
-        printf("Enter choice: ");
-        scanf("%d",&choice);
+	int item;
+	char choice;
+	printf("Press '+' to INSERT\n'-' to DELETE\n'd' to DISPLAY\n'q' to QUIT: ");
+	
+	do
+	{
+		printf("Enter choice: ");
+		scanf(" %c",&choice);
+		
 
-        switch(choice)
-        {
-            case 1: printf("Enter element: ");
-                    scanf("%d",&item);
-                    enqueue(item);
-                    break;
-            case 2: item = dequeue();
-                    if(item == INVALID)
-                        printf("Q empty\n");
-                    else    
-                        printf("Deleted item = %d\n",item);
-                    break;
-            case 3: display();
-                    break;
-            case 4: break;
-            default: printf("Invalid choice\n");
-        }
-    }while(choice != 4);
+		switch(choice)
+		{
+			case '+': printf("Enter an element: ");
+				scanf("%d",&item); 
+				enqueue(item);
+				break;
+			case '-' :item =  dequeue();
+				if(item == INVALID)
+					printf("Queue Underflow\n");
+				else
+					printf("Deleted item = %d\n",item);
+				break;
+			case 'd': display();
+				break;
+			case 'q':
+                                break;
+			default: printf("Invalid Choice\n");
+		}
+	}while(choice != 'q');
 }
