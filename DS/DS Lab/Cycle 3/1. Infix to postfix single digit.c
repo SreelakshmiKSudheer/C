701203cd16aed63008ddd42172892307c93bbc02
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-02-13 19:12:43 +0530

    Update 1. Infix to postfix single digit.c

--- DS/DS Lab/Cycle 3/1. Infix to postfix single digit.c
+++ DS/DS Lab/Cycle 3/1. Infix to postfix single digit.c
@@ -1,77 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-typedef struct
-{
-    int top;
-    char a[20];
-}stack;
+#define SIZE 20
+#define INVALID '\0'
+#define FINVALID -99999.999
 
-stack s1,s2,s3;
-char infix[20];
+float fstack[SIZE];
+char cstack[SIZE];
+int ftop = -1, ctop = -1;
 
-void push(char s[],int *top,char item)
+void fpush(float item)
 {
-        s[++(*top)] = item;
+    if(ftop == SIZE-1)
+        printf("Stack overflow\n");
+    else    
+        fstack[++ftop] = item;
 }
 
-char pop(char s[],int *top)
+float fpop()
 {
-    if(*top == -1)
-        return '\0';
-    return s[(*top)--];
+    if(ftop == -1)
+        printf("Stack underflow\n");
+    else
+        return fstack[ftop--];
 }
 
-char readsymbol(char s[])
+void cpush(char item)
 {
-    static int i = 0;
-    return s[i++];
+    if(ctop == SIZE-1)
+        printf("Stack overflow\n");
+    else    
+        cstack[++ctop] = item;
 }
 
-int isp(char op)
+char cpop()
 {
-    switch(op)
-    {
-        case '+':
-        case '-': return 2;
-        case '*':
-        case '/': return 4;
-        case '^': return 5;
-        case '(': return 0;
-        default: return -1;
-    }
-}
-
-int icp(char op)
-{
-    switch(op)
-    {
-        case '+':
-        case '-': return 1;
-        case '*':
-        case '/': return 3;
-        case '^': return 6;
-        case '(': return 7;
-        case ')': return 0;
-        default: return -1;
-    }
-}
-
-int op(char c)
-{
-    switch (c)
-    {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '^': return 1;
-            break;
-        default: return 0;
-            break;
-    }
-
+    if(ctop == -1)
+        printf("Stack underflow\n");
+    else
+        return cstack[ctop--];
 }
 
 int operand(char c)
@@ -81,130 +49,137 @@ int operand(char c)
     return 0;
 }
 
-int ATOI(char a)
+int icp(char c)
 {
-    switch(a)
+    switch(c)
     {
-        case '0': return 0;
-        case '1': return 1;
-        case '2': return 2;
-        case '3': return 3;
-        case '4': return 4;
-        case '5': return 5;
-        case '6': return 6;
-        case '7': return 7;
-        case '8': return 8;
-        case '9': return 9;
+        case '+':
+        case '-': return 1;
+
+        case '*': 
+        case '/': return 3;
+
+        case '^': return 6;
+
+        case '(': return 7;
+        case ')': return 0;
     }
 }
-int operation(char a,char b, char op)
-{
-    int A,B;
-    int result;
 
-    A = atoi(&a);
-    B = atoi(&b);
+int isp(char c)
+{
+    switch(c)
+    {
+        case '+':
+        case '-': return 2;
+
+        case '*': 
+        case '/': return 4;
+
+        case '^': return 5;
+
+        case '(': return 0;
+        
+    }
+}
+
+float calculate(float a,float b,char op)
+{
+    float result;
+
     switch(op)
     {
-        case '+': return A+B;
-        case '-': return A-B;
-        case '*': return A*B;
-        case '/': return A/B;
-        case '^': result = pow(A,B);
-                    return result; 
+        case '+': return a+b;
+        case '-': return a-b;
+        case '*': return a*b;
+
+        case '/': if(b == 0)
+                    return FINVALID;
+                  else
+                    return a/b;
+        case '^': return pow(a,b);
+
+        default : return FINVALID;
+
     }
 }
 
-void infix_to_postfix()
+int main()
 {
-    int i;
-    char item,x;
+    char infix[20],postfix[20],op,item,x;
+    int i,j,citem;
+    float result,a,b;
 
-    for(i = 0; infix[i] != '\0';i++);
+    printf("Enter an infix expression: \n");
+    scanf(" %[^\n]",infix);
 
-    infix[i++] = ')';
-    infix[i] = '\0';
-    push(s1.a,&(s1.top),'(');
+    for(i = 0; infix[i] != '\0'; i++);
+    infix[i] = ')';
 
-    while(s1.top > -1)
+    cpush('(');
+
+    i = 0;j = 0;
+
+    while(ctop >- 1)
     {
-        item = readsymbol(infix);
-        x = pop(s1.a,&(s1.top));
+        item = infix[i++];
+        x = cpop();
 
-        if(item >= '0' && item <= '9')
+        if(operand(item))
         {
-            printf("%c ",item);
-            push(s2.a,&(s2.top),item);
-            push(s1.a,&(s1.top),x);
+            postfix[j++] = item;
+            cpush(x); 
         }
         else if(item == ')')
         {
             while(x != '(')
             {
-                printf("%c ",x);
-                push(s2.a,&(s2.top),x);
-                x = pop(s1.a,&(s1.top));
+                postfix[j++] = x;
+                x = cpop();
             }
         }
         else if(isp(x) >= icp(item))
         {
             while(isp(x) >= icp(item))
             {
-                printf("%c ",x);
-                push(s2.a,&(s2.top),x);
-                x = pop(s1.a,&(s1.top));
+                postfix[j++] = x;
+                x = cpop();
             }
-            push(s1.a,&(s1.top),x);
-            push(s1.a,&(s1.top),item);
+            cpush(x);
+            cpush(item);
         }
         else if(isp(x) < icp(item))
         {
-            push(s1.a,&(s1.top),x);
-            push(s1.a,&(s1.top),item);
+            cpush(x);
+            cpush(item);
         }
     }
 
-}
+    postfix[j] = '\0';
+    printf("\nPostfix expression:\n");
+    printf("%s\n",postfix);
 
-int postfix_evaluation()
-{
-    int i,result;
-    char item,a,b;
+    postfix[j++] = '#';
+    postfix[j++] = '\0';
 
-    push(s2.a,&(s2.top),'#');
+    i = 0;
+    citem = postfix[i++];
 
-    for(i = 0; s2.a[i] != '#'; i++)
+    while(citem != '#')
     {
-        item = s2.a[i];
-        
-
-        if(operand(item))
-            push(s3.a,&(s3.top),item);
-        else if (op(item))
+        if(operand(citem))
+            fpush(citem - '0');
+        else
         {
-            a = pop(s3.a,&(s3.top));
-            b = pop(s3.a,&(s3.top));
-
-            result = operation(a,b,item);
-            push(s3.a,&(s3.top),result);
+            op = citem;
+            b = fpop();
+            a = fpop();
+            result = calculate(a,b,op);
+            fpush(result);
         }
-        
+        citem = postfix[i++];
     }
-    result = pop(s3.a,&(s3.top));
-    return result;
-}
-int main()
-{
-    int result;
-    s1.top = -1,s2.top = -1,s3.top = -1;
+    result = fpop();
+    printf("\n\nResult = %.2f\n",result);
 
-    printf("Enter an infix expression: ");
-    scanf(" %[^\n]",infix);
-
-
-    infix_to_postfix();
-
-    result = postfix_evaluation();
-
-    printf("\nThe result of the postfix expression evaluation = %d",result);
 }
