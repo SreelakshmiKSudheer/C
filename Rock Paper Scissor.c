bf880f4d06522cc0427ce27e16f85f4dbdfe5f4b
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-05-15 12:27:48 +0530

    Update Rock Paper Scissor.c

--- Rock Paper Scissor.c
+++ Rock Paper Scissor.c
@@ -5,48 +5,10 @@
 char str[20],Player_1[20],Player_2[20],Toss_1,Toss_2,choice_1,choice_2;
 int t,p1 = 0,p2 = 0,i,j,choice_valid;
 char ch;
-char* string()								// function to input string
+void string()								// function to input string
 {
-	
-	char* string = fgets(str,20,stdin);		//gets(str); // both works 
-	return string;
+	scanf(" %[^\n]",str);
 }
-
-char choice()						//r/p/s selection
-{
-	
-	scanf("  %c",&ch);
-	char C[] = {'r','R','p','P','s','S'};
-	for(j = 0;j < 6; j++)
-	{
-		choice_valid = 0;
-		if(ch == C[j])
-		{
-			choice_valid = 1;
-			break;
-		}		
-	}
-	if(choice_valid == 0)
-	{
-		printf("Invalid Choice!\nEnter Again: ");
-		choice();
-	}
-	
-	return ch;
-}
-
-
-int toss_decision()								// toss decision: who will play first
-{
-	int toss_dec_int = (rand()%2);				// rand()%n chooses a number from 0 to n randomly, excluding n
-	if(t == toss_dec_int)
-		t = 1;		
-	else
-		t = 2;
-	
-	return t;
-}
-
 
 int toss()										// Toss idal
 {
@@ -75,44 +37,90 @@ int toss()										// Toss idal
 	return 0;
 }
 
+int toss_decision()								// toss decision: who will play first
+{
+	int toss_dec_int = (rand()%2);				// rand()%n chooses a number from 0 to n randomly, excluding n
+	printf("%d\n",toss_dec_int);
+	if(t == toss_dec_int)
+		t = 1;		
+	else
+		t = 2;
+	
+	return t;
+}
 
+char choice()						//r/p/s selection
+{
+	
+	ch = getch();
+	
+	char C[] = {'r','R','p','P','s','S'};
+	for(j = 0;j < 6; j++)
+	{
+		choice_valid = 0;
+		if(ch == C[j])
+		{
+			choice_valid = 1;
+			break;
+		}		
+	}
+	if(choice_valid == 0)
+	{
+		printf("Invalid Choice!\nEnter Again: ");
+		choice();
+	}
+	
+	return ch;
+}
 
 int main()
 {
-	{												// Initialization GAME :)
-		printf("Player 1: ");
-	strcpy(Player_1,string());
-	//char* Player_1 = string();	// doesn't work
+	
+		//Initialization GAME :)
+	char players[2][50];
+		
+	printf("Player 1: ");
+	string();
+	strcpy(players[0],str);					// gets player 1 name
 	
 	printf("Player 2: ");
-	strcpy(Player_2,string());
-	//char* Player_2 = string();	// doesn't work
-	
-	printf("Players are:\nPlayer 1: %sPlayer 2: %s\n",Player_1,Player_2);
+	string();
+	strcpy(players[1],str);
+
+	sleep(1);
+	printf("Players are:\nPlayer 1: %sPlayer 2: %s\n",players[0],players[1]);
 	
 	printf("How the game works?\n\t1. 5 plays.\n\t2. Scores will be displyed after each play.\n\t3. Player with high score will be the winner.");
+	sleep(1);
 	printf("\nHow the scores are awarded?\n\t2 pts : Win\n\t1 pts : Draw\n\t0 pts : Loss");
+	sleep(1);
 	toss();
+
+	int PF = toss_decision();					// toss is random. The one whose choice matches the toss will play first		
+											
+	if(PF == 1)										// allotes first and second players
+	{
+		strcpy(Player_1,players[0]);
+		strcpy(Player_2,players[1]);
 	}
-	
-	int PF = toss_decision();
-	printf("\nPlayer %d will play first",PF);		// Who will play first
-	
-	int PS = 1;
-	if(PF == 1)
-		PS = 2;
-	
+	else
+	{
+		strcpy(Player_1,players[1]);
+		strcpy(Player_2,players[0]);
+	}	
+	printf("\n%s will play first",Player_1);
+
 	for(i = 0;i < 5;i++)							// rps
 	{
 		printf("\nRock Paper Scissor");
 		
-		printf("\nPlayer %d, enter (r/p/s): ",PF);
+		printf("\n%s, enter (r/p/s): ",Player_1);
 		choice_1 = choice();
 		
-		printf("\nPlayer %d, enter (r/p/s): ",PS);
+		printf("\n%s, enter (r/p/s): ",Player_2);
 		choice_2 = choice();
 		
-		switch(choice_1)							// score
+		switch(choice_1)							// first players choice is checked against second players and computes score
 		{
 			case 'r':
 			case 'R': if(choice_2 == 'r' || choice_2 == 'R')
@@ -168,19 +176,16 @@ int main()
 					break;
 		
 		}
-		if(PF == 1)
-			printf("Score: \nPlayer 1: %d\nPlayer 2: %d\n",p1,p2);
-		else
-			printf("Score: \nPlayer 1: %d\nPlayer 2: %d\n",p2,p1);
+		printf("Score: \n%s: %d\n%s: %d\n",Player_1,p1,Player_2,p2);
 	}
 		
 	
 		if(p1 == p2)								// Winner
 			printf("DRAW");			
 		else if(p1 > p2)
-			printf("Player %d WIns",PF);
+			printf("%s WINs",Player_1);
 		else
-			printf("Player %d WIns",PS);
+			printf("%s WINs",Player_2);
 		
 	}
 	
